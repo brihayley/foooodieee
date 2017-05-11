78c7f06e9c7593fdc8cc1bacd5356702b94cbb5f
brineids <bhneiderman@gmail.com>
2017-05-11 16:27:33 -0500

    deleted old theme files and updated foooodieee_scrush theme to use front page and edited those styles

--- wp-content/themes/businesso/footer.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Businesso Footer
- * The template for displaying the footer.
- */
-
- businesso_wp_footer ();
- 
- wp_footer(); ?>
--- wp-content/themes/foooodieee_scrush/front-page.php
+++ wp-content/themes/foooodieee_scrush/front-page.php
@@ -7,37 +7,24 @@
  * @since Skillcrush Starter 1.0
  */
 
-?>
-<!DOCTYPE html>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width">
-	<title><?php wp_title('|', true, 'right'); ?></title>
-	<link rel="profile" href="http://gmpg.org/xfn/11">
-	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,700,400' rel='stylesheet' type='text/css'>
-	<link href="https://fonts.googleapis.com/css?family=Handlee" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_url'); ?>" />
-	<?php wp_head(); ?>
-</head>
+get_header(); ?>
 
-<body <?php body_class(); ?>>
-
-<div>
-<div>
-
-<section class="home-page">
+<section class="front-page">
 	<div class="main-content">
 		<div class="content">
-			<?php while ( have_posts() ): the_post(); ?>
-				<?php the_content(); ?>
-				<div class="blog-box">
-					<div id="logo"></div>
-					<a class="blog-btn" href="<?php echo site_url('/blog/'); ?>" class="btn">
-						VIEW MY BLOG
-					</a>
+			<?php query_posts('posts_per_page=3'); ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+					<div class="single-post">
+					<h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+					<figure>
+						<?php if ( has_post_thumbnail() ) : ?>
+							<?php the_post_thumbnail('thumb'); ?>
+						<?php endif; ?>
+					</figure>
+					<?php the_excerpt(); ?>
 				</div>
-			<?php endwhile; ?>
+				<?php endwhile; ?>
+				<?php wp_reset_query(); ?>
 		</div>
 	</div>
 </section>
