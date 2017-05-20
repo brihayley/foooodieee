4fdd7b55cad94b093d4805fe2e1ee67b13ef3eb6
brineids <bhneiderman@gmail.com>
2017-05-19 19:59:08 -0500

    realligned nav and header and adjusted media queries accordingly

--- wp-content/themes/foooodieee_scrush/front-page.php
+++ wp-content/themes/foooodieee_scrush/front-page.php
@@ -10,6 +10,15 @@
 get_header(); ?>
 
 <section class="front-page">
+	<h1 class="follow">Follow me!</h1>
+	<div id="sidebar-front" class"sidebar-3">
+				<?php dynamic_sidebar( 'sidebar-3' ); ?>
+	</div>
+	<div class="media-inquiries">
+		<h3>For media inquries or brand collaboration, email
+			<a href="mailto:foooodieee@gmail.com">Foooodieee@gmail.com</a>
+		</h3>
+	</div>
 	<div class="main-content">
 		<h1 class="follow"><a href="<?php echo home_url(); ?>/blog">From the blog</a></h1>
 		<div class="content">
@@ -27,10 +36,6 @@ get_header(); ?>
 				<?php endwhile; ?>
 				<?php wp_reset_query(); ?>
 		</div>
-	</div>
-	<h1 class="follow">Follow me!</h1>
-	<div id="sidebar-front" class"sidebar-3">
-				<?php dynamic_sidebar( 'sidebar-3' ); ?>
 	</div>
 	<div id="featured" class="featured">
 		<h1>Featured in...</h1>
